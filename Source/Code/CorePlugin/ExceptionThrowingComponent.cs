2163db8e1c4c43f840a196d376da91f0f1148dba
Andrea <roundcrisis@gmail.com>
2015-01-06 16:25:09 +0000

    source for the sample

---
+++ Source/Code/CorePlugin/ExceptionThrowingComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace exceptionlessTest
+{
+	[Serializable]
+    public class ExceptionThrowingComponent : Component, ICmpInitializable
+    {
+
+		public void OnInit(InitContext context)
+		{
+			if (context != InitContext.Activate || DualityApp.ExecContext != DualityApp.ExecutionContext.Game)
+				return;
+
+			Log.Game.Write("Hi throwing an exception for exceptionless");
+
+			throw new Exception("Exception test ");
+			
+		}
+
+		public void OnShutdown(ShutdownContext context)
+		{
+	
+		}
+    }
+}
